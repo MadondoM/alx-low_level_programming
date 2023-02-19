6e95333b83f9fb47ae8fa0b93ced023fe3b37f68
MadondoM <madiemadoh09@gmail.com>
2023-02-19 11:29:02 -0800

    2. Print alphabet

---
+++ 2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Programe that prints out programe in lowercase
+ *
+ * Return : 0 (Success)
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; l++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
