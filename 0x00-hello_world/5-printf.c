ae0e609b42272f46517169964e21a6e62d3f8105
“MadondoM” <madiemadoh09@gmail.comgit config --global user.email madiemadoh09@gmail.com>
2023-05-18 14:12:49 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print with proper grammar, but the outcome is a piece of art,
+ * followed by a new line.
+ * Return: 0 Always(success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
