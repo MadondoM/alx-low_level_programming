1add0c31d2843e54550fb27ee280a5ec26cc0c4f
“MadondoM” <madiemadoh09@gmail.comgit config --global user.email madiemadoh09@gmail.com>
2023-05-19 14:59:39 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 48; m < 58; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
