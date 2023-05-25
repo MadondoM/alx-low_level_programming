c4ba532e5c205219cb4665cdc22ec84e4dea9900
“MadondoM” <madiemadoh09@gmail.comgit config --global user.email madiemadoh09@gmail.com>
2023-05-25 16:51:02 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - Program to test function for positive or negative
+* Return: 0(Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
