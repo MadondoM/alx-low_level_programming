1375a818a5d3717733e15af03fe74a673983cbc6
“MadondoM” <madiemadoh09@gmail.comgit config --global user.email madiemadoh09@gmail.com>
2023-05-25 18:11:34 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiply 2 int
+ * @l: first int
+ * @p: second int
+ * Return: addition of the multiplicattion
+ */
+
+int mul(int l, int p)
+{
+	return (l * p);
+}
